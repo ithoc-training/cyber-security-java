ede60632a922446f73551e54755edd04c4d2d893
Oli Hock <oliver.hock@gmail.com>
2023-11-06 21:50:10 +0100

    Add RestTemplate to UrlCheckController to make HTTP GET request

--- src/main/java/de/ithoc/training/cybersecurity/ssrf/UrlCheckController.java
+++ src/main/java/de/ithoc/training/cybersecurity/ssrf/UrlCheckController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class UrlCheckController {
@@ -19,7 +20,15 @@ public class UrlCheckController {
     @PostMapping("/check")
     public String check(@RequestParam("url") String url, Model model) {
 
-        // TODO Implement me
+        RestTemplate restTemplate = new RestTemplate();
+        String content;
+        try {
+            content = restTemplate.getForObject(url, String.class);
+        } catch (Exception e) {
+            content = "Error: " + e.getMessage();
+        }
+
+        model.addAttribute("content", content);
 
         return "ssrf/url-check";
     }
