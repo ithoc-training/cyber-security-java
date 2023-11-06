590c1586fe412c76929fa9ad965684ea55c4bb36
Oli Hock <oliver.hock@gmail.com>
2023-11-06 22:24:00 +0100

    Add Cross-origin Resource Sharing (CORS) exercise files.

---
+++ src/main/java/de/ithoc/training/cybersecurity/cors/Employee.java
@@ -0,0 +1,15 @@
+package de.ithoc.training.cybersecurity.cors;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+
+    private String id;
+    private String email;
+    private String firstname;
+    private String surname;
+    private String department;
+    private String position;
+
+}
